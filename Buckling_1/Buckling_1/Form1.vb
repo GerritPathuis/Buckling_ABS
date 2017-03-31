1afbb35b9d9101ec74279df021e99145c1197e13
GerritPathuis <g.pathuis@quicknet.nl>
2017-03-31 15:14:32 +0200

    Update

--- Buckling_1/Buckling_1/Form1.vb
+++ Buckling_1/Buckling_1/Form1.vb
@@ -13,42 +13,55 @@ Public Class Form1
     Public _σCx As Double       'maximum compressive stress in longitudinal direction
     Public _σCy As Double       'maximum compressive stress in transverse direction
 
-    '-------- yield points ----------------
+    '-------- Material ----------------
     Public _σ0 As Double        'specified minimum yield point of plate [N/cm2]
     Public _τ0 As Double        'shear strength plate
+    Public _E As Double         'Elasticity
+    Public _v As Double         'Poissons ratio
 
     '-------- shear stress -------
-    Public _τ As Double         'Edge shear stress
     Public _τC As Double        'Edge shear stress
-
     Public _η As Double = 1     'max length utilizing factor
 
     '------- loads--------
-    Public _q As Double         'Uniform lateral load [N/cm2]
+    Public _σax, _σay As Double     'Uniforn in-plane compression
+    Public _σbx, _σby As Double     'Uniforn in-plane bending
+    Public _q As Double             'Uniform lateral load [N/cm2]
+    Public _τ As Double             'Edge shear stress
+
+    '------- dimensions --------
     Public _t As Double         'Plate thickness [mm]
     Public _S As Double         'Stiffeners distance [mm]
     Public _L As Double         'stiffeners length [mm]
     Public _LG As Double        'girder length [mm]
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Read_material()
+        Calc_sequence()
     End Sub
 
-    Private Sub Read_material()
+    Private Sub Read_dimensions()
         _L = NumericUpDown1.Value   'Length
         _S = NumericUpDown2.Value   'stiffener distance
         _t = NumericUpDown3.Value   'plate thicknes
-
     End Sub
     Private Sub Read_loads()
         _q = NumericUpDown6.Value   'Uniform lateral load [N/cm2]
-        _S = NumericUpDown5.Value   'Comp stress in longit direction
-        _t = NumericUpDown4.Value   'plate thicknes
+        _σax = NumericUpDown5.Value 'Comp stress in longit direction
+        _σay = NumericUpDown4.Value 'Comp stress in travesre direction
 
+        _σbx = NumericUpDown9.Value 'Bending stress in longit direction
+        _σby = NumericUpDown8.Value 'Bending stress in traverse direction
+
+        _τ = NumericUpDown7.Value   'Edge shear stress
+
+        _σxmax = NumericUpDown11.Value  'maximum compressive stress in longitudinal direction
+        _σymax = NumericUpDown10.Value  'maximum compressive stress in transverse direction
     End Sub
     Private Sub Read_properties()
         _σ0 = NumericUpDown14.Value
         _τ0 = _σ0 / Sqrt(3)
+        _E = NumericUpDown13.Value
+        _v = NumericUpDown12.Value
     End Sub
 
     'See page 27
@@ -69,7 +82,7 @@ Public Class Form1
     End Sub
 
     Private Sub Calc_sequence()
-        Read_material()
+        Read_dimensions()
         Read_properties()
         Read_loads()
         Calc_chaper3_1()
